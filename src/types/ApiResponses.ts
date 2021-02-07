9eec53468ea91b7989b5cc6ca40a49381a94620c
lewjc <lewisjcummins@hotmail.co.uk>
2021-02-07 15:01:01 +0000

    Update api routes and responeses in relation to updates to the aggregator

--- src/types/ApiResponses.ts
+++ src/types/ApiResponses.ts
@@ -1,5 +1,22 @@
+import { Settings } from "./Settings";
 import { Service } from "./Service";
+import { ColourRule } from "react-combilazylog";
 
 export type GetServicesResponse = {
 	services: Service[];
+	message: string;
+};
+
+export type GetSettingsResponse = {
+	settings: Settings;
+	message?: string;
+};
+
+export type GetColourRules = {
+	colourRules: ColourRule[];
+	message?: string;
+};
+
+export type DeleteColourRule = {
+	message: string;
 };
