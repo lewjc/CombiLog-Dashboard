00f7f6eaf4bfb54cc5f4a608c002bba9f856d026
Lewis Cummins <lewisjcummins@hotmail.co.uk>
2021-10-31 11:43:06 +0000

    Fix prod script

--- env.sh
+++ env.sh
@@ -11,7 +11,7 @@ if [ $DEV ]; then
   echo "}" >> ./env-config.js
 else
   echo "window._env_ = {" > /usr/share/nginx/html/env-config.js
-  awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' /usr/share/nginx/html/.env >> /usr/share/nginx/html/env-config.js
+  awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' /usr/share/nginx/.env >> /usr/share/nginx/html/env-config.js
   echo "}" >> /usr/share/nginx/html/env-config.js
 fi
 
